a1d2b5d19f10ae0a405696b175c24787c025d6ab
Jack <jack.naisbett@gmail.com>
2021-02-22 11:30:13 +0000

    Merge separate script files as functions

--- vnstat-on-merlin.sh
+++ vnstat-on-merlin.sh
@@ -126,3 +126,53 @@ Mount_WebUI(){
 
 Create_Dirs
 Mount_WebUI
+
+vnstat_ww(){
+	# This script is used to refresh the daily/weekly/monthly vnstati - images - usage for the Vnstat on Merlin UI - by dev_null at snbforums
+	# Adapted from http://code.google.com/p/x-wrt/source/browse/trunk/package/webif/files/www/cgi-bin/webif/graphs-vnstat.sh
+	# mkdir /www/user/vnstat && cp /jffs/scripts/vnstat.htm /www/user/vnstat/vnstat.htm # not needed for Vnstat on Merlin
+	logger -s -t vnstats vnstati updating stats for UI
+	vnstat -u
+	#
+	WWW_D=/www/user/vnstat # output images to here
+	LIB_D=/opt/var/lib/vnstat # db location - verify matches your install
+	BIN=/opt/bin/vnstati  # which vnstati - verify matches your install
+	#
+	outputs="s h d t m hs"   # what images to generate
+	#
+	# Sanity checks
+	[ -d "$WWW_D" ] || mkdir -p "$WWW_D" # make the folder if it dont exist.
+	#
+	# You might want to setup a link if it dont exist.
+	# [ -L /www/vnstat ] || ln -sf /www/vnstat /www/user/
+	# was /tmp/www/
+	#
+	# End of config changes
+	interfaces="eth0"
+	# was "$(ls -1 $LIB_D)"
+
+	if [ -z "$interfaces" ]; then
+	    echo "No database found, nothing to do."
+	    echo "A new database can be created with the following command: "
+	    echo "    vnstat -u -i eth0"
+	    exit 0
+	else
+	    for interface in $interfaces ; do
+	        for output in $outputs ; do
+	            $BIN -${output} -i $interface -o $WWW_D/vnstat_${output}.png
+	        done
+	    done
+	fi
+}
+
+vnstat_stats(){
+	# This script is used to create the daily/weekly/monthly vnstat usage for the Vnstat on Merlin script and UI - by dev_null at snbforums
+	printf "\\nVnstats as of:\\n%s" "$(date)" >> /home/root/vnstat.txt
+	/opt/bin/vnstat -m >> /home/root/vnstat.txt
+	/opt/bin/vnstat -w >> /home/root/vnstat.txt
+	/opt/bin/vnstat -d >> /home/root/vnstat.txt
+	echo >> /home/root/vnstat.txt
+	cat /home/root/vnstat.txt
+	cat /home/root/vnstat.txt | convert -font DejaVu-Sans-Mono -channel RGB -negate label:@- /tmp/var/wwwext/vnstat/vnstat.png
+	logger -s -t vnstat_totals summary generated
+}
