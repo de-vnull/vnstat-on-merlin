600f7d8d83d91ae02bf8c236fcdcba56c0c7642e
Jack Naisbett <jack.naisbett@gmail.com>
2021-08-15 22:29:52 +0100

    Remove day from MonthFormat

--- dn-vnstat.sh
+++ dn-vnstat.sh
@@ -443,8 +443,8 @@ Conf_Exists(){
 			sed -i 's/^OutputStyle.*$/OutputStyle 0/' "$SCRIPT_STORAGE_DIR/vnstat.conf"
 			restartvnstat="true"
 		fi
-		if ! grep -q '^MonthFormat "%Y-%m ('"$(AllowanceStartDay check | awk '{printf("%02d", $1)}')"')"' "$SCRIPT_STORAGE_DIR/vnstat.conf"; then
-			sed -i 's/^MonthFormat.*$/MonthFormat "%Y-%m ('"$(AllowanceStartDay check | awk '{printf("%02d", $1)}')"')"/' "$SCRIPT_STORAGE_DIR/vnstat.conf"
+		if ! grep -q '^MonthFormat "%Y-%m"$' "$SCRIPT_STORAGE_DIR/vnstat.conf"; then
+			sed -i 's/^MonthFormat.*$/MonthFormat "%Y-%m"/' "$SCRIPT_STORAGE_DIR/vnstat.conf"
 			restartvnstat="true"
 		fi
 		
